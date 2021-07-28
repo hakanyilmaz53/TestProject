0c42ec004e407cae11810cc0a3cfdcaf73023252
hakan5335 <hakannyilmaz53@gmail.com>
2021-07-28 22:04:21 +0300

    Initial commit

---
+++ TestProject/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TestProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.baslangicTarihiTextBox = new System.Windows.Forms.TextBox();
+            this.bitisTarihiTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.malKoduTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // baslangicTarihiTextBox
+            // 
+            this.baslangicTarihiTextBox.Location = new System.Drawing.Point(406, 19);
+            this.baslangicTarihiTextBox.Name = "baslangicTarihiTextBox";
+            this.baslangicTarihiTextBox.Size = new System.Drawing.Size(143, 20);
+            this.baslangicTarihiTextBox.TabIndex = 0;
+            // 
+            // bitisTarihiTextBox
+            // 
+            this.bitisTarihiTextBox.Location = new System.Drawing.Point(406, 53);
+            this.bitisTarihiTextBox.Name = "bitisTarihiTextBox";
+            this.bitisTarihiTextBox.Size = new System.Drawing.Size(143, 20);
+            this.bitisTarihiTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(318, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Başlangıç Tarihi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bitiş Tarihi";
+            // 
+            // malKoduTextBox
+            // 
+            this.malKoduTextBox.Location = new System.Drawing.Point(112, 22);
+            this.malKoduTextBox.Name = "malKoduTextBox";
+            this.malKoduTextBox.Size = new System.Drawing.Size(143, 20);
+            this.malKoduTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mal Kodu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(695, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "GETİR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(743, 294);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.malKoduTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bitisTarihiTextBox);
+            this.Controls.Add(this.baslangicTarihiTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox baslangicTarihiTextBox;
+        private System.Windows.Forms.TextBox bitisTarihiTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox malKoduTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ TestProject/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestProject
+{
+    public partial class Form1 : Form
+    {
+        TestEntities context = new TestEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime basTarihDate = baslangicTarihiTextBox.Text == "" ? DateTime.Now : DateTime.Parse(baslangicTarihiTextBox.Text);
+            DateTime bitTarihDate = bitisTarihiTextBox.Text == "" ? DateTime.Now.AddDays(10) : DateTime.Parse(bitisTarihiTextBox.Text);
+
+            int baslangicTarih = Convert.ToInt32(basTarihDate.ToOADate());
+            int bitisTarih = Convert.ToInt32(bitTarihDate.ToOADate());
+
+            var procedureList = context.ProcedureTest(malKoduTextBox.Text, baslangicTarih, bitisTarih).ToList();
+            dataGridView1.DataSource = procedureList;
+
+        }
+
+
+    }
+}
